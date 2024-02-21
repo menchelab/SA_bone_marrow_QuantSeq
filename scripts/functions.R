a22665dff9096e319e707f337ddb1a313c12ff08
annahyan <anna@menchelab.com>
2024-02-22 00:10:56 +0100

    updates

--- scripts/functions.R
+++ scripts/functions.R
@@ -121,3 +121,44 @@ add_mouse_geneinfo = function(filename, suffix = "gene_info", entrez.info.file =
     
     write_excel_allsheets(file.sheets.list, filename = outfile)
 }
+
+
+
+library(dplyr)
+# mouse_human_genes = read.csv("http://www.informatics.jax.org/downloads/reports/HOM_MouseHumanSequence.rpt",sep="\t")
+
+## https://www.biostars.org/p/9567892/#9568018
+
+convert_mouse_to_human <- function(gene_list) { 
+    output = c()
+    
+    for(gene in gene_list) {
+        class_key = (mouse_human_genes %>% filter(Symbol == gene & Common.Organism.Name == "mouse, laboratory"))[['DB.Class.Key']]
+        if( !identical(class_key, integer(0)) ) {
+            human_genes = (mouse_human_genes %>% filter(DB.Class.Key == class_key & Common.Organism.Name=="human"))[,"Symbol"]
+            for(human_gene in human_genes) {
+                output = rbind(c(gene, human_gene), output)
+            }
+        }
+    }
+    return (output)
+}
+
+convert_human_to_mouse <- function(gene_list) {
+    output = c()
+    
+    if (!exists("mouse_human_genes")) {
+        mouse_human_genes <<- read.csv("https://www.informatics.jax.org/downloads/reports/HOM_MouseHumanSequence.rpt",sep="\t")    
+    }
+    
+    for(gene in gene_list) {
+        class_key = (mouse_human_genes %>% filter(Symbol == gene & Common.Organism.Name == "human"))[['DB.Class.Key']]
+        if( !identical(class_key, integer(0)) ) {
+            human_genes = (mouse_human_genes %>% filter(DB.Class.Key == class_key & Common.Organism.Name=="mouse, laboratory"))[,"Symbol"]
+            for(human_gene in human_genes) {
+                output = rbind(c(gene, human_gene), output)
+            }
+        }
+    }
+    return (output)
+}
